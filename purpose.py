fa4d8b10c5811b620179839a24dab8fd6d4d27a8
ktmora <katherinemora@berkeley.edu>
2026-03-02 17:54:19 -0800

    Addind file: purpose.py that states the purpose of making a new repo

---
+++ purpose.py
@@ -0,0 +1,9 @@
+# File: purpose.py
+
+"""
+The purpose of this git repo is that we can push and pull just the jupyter notebooks and any useful packages
+wihout having to constantly push all of our data.
+
+Hope this helps.
+-Katie
+"""
